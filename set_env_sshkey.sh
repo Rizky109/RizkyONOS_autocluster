6916cdfafdd2d4c9e6b0daf01c0bb5f753a59fa3
Achmad Basuki <9971912+abazh@users.noreply.github.com>
2021-05-18 09:13:41 +0700

    Update set_env_sshkey.sh
    adding information

--- set_env_sshkey.sh
+++ set_env_sshkey.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-
+echo "Creating RSA SSH-key pair, if already exist you may press n"
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
 USER=$(whoami)
 SSH_KEY=$(cut -d\  -f2 ~/.ssh/id_rsa.pub)
 
-	echo "Insert onos-user-key to all controllers..."
+echo "Inserting RSA pubkey to all controllers..."
 for i in {1..2}; do
 	docker exec -it onos$i bin/onos-user-key $USER $SSH_KEY
 done
